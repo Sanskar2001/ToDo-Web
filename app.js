250660e09e3992645eaeb3bc5e0a0112a79b281d
Sanskar2001 <sanskaratrey22@gmail.com>
2021-11-30 19:50:03 +0530

    Added images

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser= require('body-parser');
+const mongoose=require('mongoose')
 const app=express();
 var items=["1","2","3"];
 
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true})
+
+const tasksSchema=new mongoose.Schema({
+    task:String
+})
+const Todos=mongoose.model("Todos",tasksSchema)
+
+
 
 app.set("view engine","ejs")
 var today= new Date();
@@ -56,6 +65,10 @@ app.get('/',(req,res)=>{
 app.post('/',(req,res)=>{
     console.log(req.body.txtbox)
      item=req.body.txtbox
+     const task=new Todos({
+        task:item
+    })
+    task.save();
      items.push(item)
     res.redirect("/")
     
